0471fa8c559ff6c501f36a7282274952ceb36abf
velychka <nastyavelychko@gmail.com>
2020-07-07 15:04:35 +0200

    #6

--- server.js
+++ server.js
@@ -12,6 +12,27 @@ app.get('/', (req, res)=> {
         res.status(200).json({"serveur" : "ok"});
 })
 
+// ------------------------
+// ROUTES RESOURCES
+// ------------------------
+
+var recettes_salade = [{
+	"id" : "1",
+	"nom" : "cesar",
+	"description" : "préparation de la salade césar",
+    "ingredients" : [ "poulet", "feuille_salade", "tomate"],
+    "photo" : "1829913150257"
+}]
+
+
+app.get('/recettes',(req, res)=>{
+	res.status(200).json(recettes_salade)
+})
+
+
+app.get('/recettes/:idRecette',(req, res)=>{
+	res.status(200).json(recettes_salade.find(recettes_salade => recettes_salade.id == req.params.idRecette))
+})
 
 // ------------------------
 // START SERVER
